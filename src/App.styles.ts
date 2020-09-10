4eebb7816f8eb6fa1f750f3dc375d514db868d33
tracht <marija.trachtenberg@gmail.com>
2020-09-10 14:58:40 +0100

    Reduces margin to 0.5em for answer button choices

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -56,7 +56,7 @@ export const Button =  styled.button`
   background: "palevioletred";
   color: "white" ;
   font-size: 1em;
-  margin: 1em;
+  margin: 0.5em;
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
   border-radius: 3px;
